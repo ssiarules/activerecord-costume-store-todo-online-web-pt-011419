0780edc23115a2eb7d9e83063c24989470457296
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-07 16:37:41 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.string "theme"
+    t.string "themem"
     t.integer "price"
     t.boolean "family_friendly"
     t.datetime "opening_time"
