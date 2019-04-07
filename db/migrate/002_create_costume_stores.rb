482d46ffa4b74a11466f9e3c22f6fe760b1ba065
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-07 16:25:38 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       create_table :costumes do |t|
       t.string :name
       t.string :location 
-      t.integer :size 
+      t.integer :costume_inventory
       t.string :image_url 
       t.timestamp :created_url
       t.timestamp :updated_at
