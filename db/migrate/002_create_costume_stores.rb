4fddad9e619d4e7810afd7c2ce5473f2cb348076
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-07 16:25:26 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
    def change
       create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.string :location 
       t.integer :size 
       t.string :image_url 
       t.timestamp :created_url
