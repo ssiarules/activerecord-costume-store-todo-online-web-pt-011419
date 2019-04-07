4c9b453c43da5d49b294111ebc9ed2d17bae1dae
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-07 16:27:56 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+ 
+   def change
+      create_table :costumes do |t|
+      t.string :name
+      t.string :location 
+      t.integer :costume_inventory
+      t.integer :employees
+      t.boolean :in_business
+      t.datetime:opening_time
+      t.datetime:closing_time
+      end
+  end 
+end 
